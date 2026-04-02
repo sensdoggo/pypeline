ac147f182bf42c61fef9c75538afc0e78914903a
Rei Armenia <5091445+sensdoggo@users.noreply.github.com>
2026-04-02 14:41:04 -0400

    typer to main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from pypeline!")
+import typer
 
+app = typer.Typer()
+
+@app.command()
+def main():
+    print("hello from pypeline")
 
 if __name__ == "__main__":
-    main()
+    app()
